848c48cc3bb2878918ab32324af44cd3e97ce436
fabio <fabiosoldado@gmail.com>
2023-02-12 18:48:54 +0000

    Implemented MarsRoverActionPerformer

---
+++ src/MarsRoverActionPerformer.test.ts
@@ -0,0 +1,220 @@
+import { MarsRoverState } from './types'
+import { CardinalDirection, RobotAction } from './constants'
+import { MarsRoverActionPerformer } from './MarsRoverActionPerformer'
+
+describe('MarsRover', () => {
+  describe('state', () => {
+    const actionPerformer = new MarsRoverActionPerformer({
+      maxLatitude: 4,
+      maxLongitude: 8,
+    })
+    test('succeeds when called with valid start position', () => {
+      expect(() =>
+        actionPerformer.state(2, 3, CardinalDirection.East)
+      ).not.toThrow()
+      expect(actionPerformer.state(2, 3, CardinalDirection.East)).toStrictEqual(
+        <MarsRoverState>{
+          orientation: CardinalDirection.East,
+          position: {
+            x: 2,
+            y: 3,
+          },
+          isLost: false,
+        }
+      )
+      expect(() =>
+        actionPerformer.state(0, 0, CardinalDirection.East)
+      ).not.toThrow()
+      expect(actionPerformer.state(0, 0, CardinalDirection.East)).toStrictEqual(
+        <MarsRoverState>{
+          orientation: CardinalDirection.East,
+          position: {
+            x: 0,
+            y: 0,
+          },
+          isLost: false,
+        }
+      )
+    })
+    test('fails when called with invalid start position', () => {
+      expect(() =>
+        actionPerformer.state(4, 9, CardinalDirection.East)
+      ).toThrow()
+      expect(() =>
+        actionPerformer.state(5, 8, CardinalDirection.East)
+      ).toThrow()
+    })
+  })
+  describe('performAction', () => {
+    const actionPerformer = new MarsRoverActionPerformer({
+      maxLatitude: 4,
+      maxLongitude: 8,
+    })
+    const initialState: MarsRoverState = {
+      orientation: CardinalDirection.North,
+      position: {
+        x: 2,
+        y: 3,
+      },
+      isLost: false,
+    }
+    describe('when robot is not lost', () => {
+      test('rotate left', () => {
+        expect(
+          actionPerformer.performAction(initialState, RobotAction.RotateLeft)
+        ).toStrictEqual(<MarsRoverState>{
+          ...initialState,
+          orientation: CardinalDirection.West,
+        })
+      })
+      test('rotate right', () => {
+        expect(
+          actionPerformer.performAction(initialState, RobotAction.RotateRight)
+        ).toStrictEqual(<MarsRoverState>{
+          ...initialState,
+          orientation: CardinalDirection.East,
+        })
+      })
+      test('moves forward to a valid position', () => {
+        const expected: MarsRoverState = {
+          ...initialState,
+          position: {
+            x: 2,
+            y: 4,
+          },
+          isLost: false,
+        }
+        expect(
+          actionPerformer.performAction(initialState, RobotAction.MoveForward)
+        ).toStrictEqual(expected)
+      })
+      test('marks lost and keeps previous coordinates when moving forward to an invalid position', () => {
+        const state: MarsRoverState = {
+          ...initialState,
+          position: {
+            ...initialState.position,
+            y: 8,
+          },
+        }
+        expect(
+          actionPerformer.performAction(state, RobotAction.MoveForward)
+        ).toStrictEqual(<MarsRoverState>{
+          ...state,
+          isLost: true,
+        })
+      })
+    })
+    describe('when robot is lost', () => {
+      test('keeps coordinates if the status is already lost', () => {
+        const state: MarsRoverState = { ...initialState, isLost: true }
+        expect(
+          actionPerformer.performAction(state, RobotAction.MoveForward)
+        ).toStrictEqual(<MarsRoverState>{
+          ...state,
+        })
+      })
+    })
+  })
+  describe('perform actions', () => {
+    const actionPerformer = new MarsRoverActionPerformer({
+      maxLatitude: 4,
+      maxLongitude: 8,
+    })
+    test('performs all actions and returns final state', () => {
+      const initialState = actionPerformer.state(2, 3, CardinalDirection.East)
+      const actions = [
+        RobotAction.RotateLeft,
+        RobotAction.MoveForward,
+        RobotAction.RotateRight,
+        RobotAction.MoveForward,
+        RobotAction.MoveForward,
+      ]
+      expect(
+        actionPerformer.performActions(initialState, actions)
+      ).toStrictEqual(<MarsRoverState>{
+        ...initialState,
+        position: {
+          x: 4,
+          y: 4,
+        },
+        orientation: CardinalDirection.East,
+        isLost: false,
+      })
+    })
+    test('performs all actions and returns final state (2)', () => {
+      const initialState = actionPerformer.state(2, 3, CardinalDirection.North)
+      const actions = [
+        RobotAction.MoveForward,
+        RobotAction.RotateLeft,
+        RobotAction.RotateLeft,
+        RobotAction.MoveForward,
+        RobotAction.RotateRight,
+      ]
+      expect(
+        actionPerformer.performActions(initialState, actions)
+      ).toStrictEqual(<MarsRoverState>{
+        ...initialState,
+        position: {
+          x: 2,
+          y: 3,
+        },
+        orientation: CardinalDirection.West,
+        isLost: false,
+      })
+    })
+    describe('when the robot goes off grid', () => {
+      test('returns final known state', () => {
+        const initialState = actionPerformer.state(
+          0,
+          2,
+          CardinalDirection.North
+        )
+        const actions = [
+          RobotAction.MoveForward,
+          RobotAction.MoveForward,
+          RobotAction.RotateLeft,
+          RobotAction.MoveForward,
+          RobotAction.RotateRight,
+          RobotAction.MoveForward,
+          RobotAction.MoveForward,
+        ]
+        expect(
+          actionPerformer.performActions(initialState, actions)
+        ).toStrictEqual(<MarsRoverState>{
+          ...initialState,
+          position: {
+            x: 0,
+            y: 4,
+          },
+          orientation: CardinalDirection.West,
+          isLost: true,
+        })
+      })
+      test('returns final known state (2)', () => {
+        const initialState = actionPerformer.state(
+          1,
+          0,
+          CardinalDirection.South
+        )
+        const actions = [
+          RobotAction.MoveForward,
+          RobotAction.MoveForward,
+          RobotAction.RotateRight,
+          RobotAction.RotateLeft,
+          RobotAction.MoveForward,
+        ]
+        expect(
+          actionPerformer.performActions(initialState, actions)
+        ).toStrictEqual(<MarsRoverState>{
+          ...initialState,
+          position: {
+            x: 1,
+            y: 0,
+          },
+          orientation: CardinalDirection.South,
+          isLost: true,
+        })
+      })
+    })
+  })
+})
---
+++ src/MarsRoverActionPerformer.ts
@@ -0,0 +1,106 @@
+import ActionPerformer from './ActionPerformer'
+import {
+  MarsRoverAction,
+  MarsRoverOrientation,
+  MarsRoverPosition,
+  MarsRoverState,
+  MarsRoverWorldDimensions,
+} from './types'
+import { RobotAction, RotationDirection } from './constants'
+import { CardinalOrientationHelper } from './utils/CardinalOrientationHelper'
+
+export class InvalidPositionError extends RangeError {
+  constructor() {
+    super('Invalid position')
+  }
+}
+
+/** Action performer for the Mars Rover robot */
+export class MarsRoverActionPerformer extends ActionPerformer<
+  MarsRoverWorldDimensions,
+  MarsRoverPosition,
+  MarsRoverOrientation,
+  MarsRoverAction
+> {
+  constructor(worldDimensions: MarsRoverWorldDimensions) {
+    super(worldDimensions)
+  }
+
+  /** Returns a state based on the given coordinates and orientation. Throws an InvalidPositionError if the
+   * coordinates are not valid for the current World dimensions */
+  state(
+    x: number,
+    y: number,
+    orientation: MarsRoverOrientation
+  ): MarsRoverState {
+    const position: MarsRoverPosition = { x, y }
+    if (!this.isValidPosition(position)) {
+      throw new InvalidPositionError()
+    }
+
+    return {
+      position,
+      orientation,
+      isLost: false,
+    }
+  }
+
+  performAction(
+    state: MarsRoverState,
+    action: MarsRoverAction
+  ): MarsRoverState {
+    if (state.isLost) {
+      return state
+    }
+    switch (action) {
+      case RobotAction.MoveForward:
+        return this.moveForward(state)
+      case RobotAction.RotateLeft:
+        return this.rotate(state, RotationDirection.Left)
+      case RobotAction.RotateRight:
+        return this.rotate(state, RotationDirection.Right)
+    }
+  }
+
+  protected moveForward(state: MarsRoverState): MarsRoverState {
+    const { xOffset, yOffset } =
+      CardinalOrientationHelper.getMoveForwardOffsets(state.orientation, 1)
+    const newPosition: MarsRoverPosition = {
+      x: state.position.x + xOffset,
+      y: state.position.y + yOffset,
+    }
+
+    if (this.isValidPosition(newPosition)) {
+      return {
+        ...state,
+        position: newPosition,
+      }
+    } else {
+      return {
+        ...state,
+        isLost: true,
+      }
+    }
+  }
+
+  protected rotate(
+    state: MarsRoverState,
+    direction: RotationDirection
+  ): MarsRoverState {
+    const currentOrientation = state.orientation
+    const nextOrientation = CardinalOrientationHelper.rotate(
+      currentOrientation,
+      direction
+    )
+    return {
+      ...state,
+      orientation: nextOrientation,
+    }
+  }
+
+  protected isValidPosition(position: MarsRoverPosition): boolean {
+    const { maxLatitude, maxLongitude } = this.worldDimensions
+    const { x, y } = position
+    return x <= maxLatitude && x >= 0 && y <= maxLongitude && y >= 0
+  }
+}
